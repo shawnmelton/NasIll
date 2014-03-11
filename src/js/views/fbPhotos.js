403b4b0206621157683a1ba450e097da08539597
Shawn Melton <shawn.a.melton@gmail.com>
2014-03-11 07:34:39 -0400

    Adding FB Albums and pictures.

--- src/js/views/fbPhotos.js
+++ src/js/views/fbPhotos.js
@@ -4,33 +4,27 @@ define(['jquery', 'backbone', 'templates/jst', 'models/user', 'models/albumCover
         el: "#content",
         rendered: false,
         section: null,
+        breadcrumb: null,
+        photoContainer: null,
+        albumHTML: '',
+        albums: [],
         events: {
             'click #fbpSelectLink': 'onPhotoSelectClick',
             'click #fbpUploadLink': 'onUploadClick'
         },
 
+        hideBreadcrumb: function() {
+            this.breadcrumb.style.display = 'none';
+        },
+
         loadPhotos: function() {
             var _this = this;
             FB.api('/me/albums', {
-                fields: 'id, photos, name'
-            }, function(response) {
-                var html = '';
-                _.each(response.data, function(album) {
-                    if(album.name === 'Profile Pictures') {
-                        _.each(album.photos.data, function(photo) {
-                            html += '<a class="fbPic"><img src="'+ photo.picture +'" alt="'+ photo.source +'" fullwidth="'+ photo.width +'"></a>';
-                        });
-                    }
-                });
-
-                if(html !== '') {
-                    $(document.getElementById('photosContainer')).html(html);
-                    $('a.fbPic').click(function() {
-                        $('a.fbPic').removeClass('selected');
-                        $(this).addClass('selected');
-                        _this.onPhotoClick($(this).find('img'));
-                    });
-                }
+                fields: 'id, name, count, picture',
+                limit: 100
+            }, function(r) {
+                _this.albums = r.data;
+                _this.showAlbums();
             });
         },
 
@@ -58,6 +52,11 @@ define(['jquery', 'backbone', 'templates/jst', 'models/user', 'models/albumCover
             if(!this.rendered) {
                 this.rendered = true;
                 this.$el.append(JST['src/js/templates/fbPhotos.html']());
+
+                this.breadcrumb = document.getElementById('fbBreadcrumb');
+                this.photoContainer = $(document.getElementById('photosContainer'));
+
+                this.setBreadcrumbEvents();
                 this.loadPhotos();
             } else {
                 this.section.fadeIn();
@@ -87,10 +86,67 @@ define(['jquery', 'backbone', 'templates/jst', 'models/user', 'models/albumCover
             });
         },
 
+        setBreadcrumbEvents: function() {
+            var _this = this;
+            $(this.breadcrumb).find('a').click(function(ev) {
+                ev.preventDefault();
+                _this.showAlbums();
+            });
+        },
+
         setSection: function() {
             if(this.section === null) {
                 this.section = $(document.getElementById('fbPhotos'));
             }
+        },
+
+        showAlbums: function() {
+            this.photoContainer.html('<span>Loading ...</span>');
+            this.hideBreadcrumb();
+
+            var _this = this;
+            if(this.albumHTML === '') {
+                _.each(this.albums, function(album) {
+                    _this.albumHTML += '<a class="fbAlbum" rel="'+ album.id +'"><img src="'+ album.picture.data.url +'" alt="'+ album.name +'"><span>'+ album.name +'</span></a>';
+                });
+            }
+
+            if(this.albumHTML !== '') {
+                this.photoContainer.html(this.albumHTML);
+                $('a.fbAlbum').click(function() {
+                    _this.showAlbumPhotos($(this).attr('rel'), $(this).find('span').html());
+                });
+            }
+        },
+
+        showAlbumPhotos: function(albumId, albumName) {
+            this.photoContainer.html('<span>Loading ...</span>');
+            this.showBreadcrumb(albumName);
+
+            var html = '',
+                _this = this;
+            FB.api('/'+ albumId +'/photos', {
+                fields: 'picture, source, width',
+                limit: 100
+            }, function(r) {
+                _.each(r.data, function(photo) {
+                    html += '<a class="fbPic"><img src="'+ photo.picture +'" alt="'+ photo.source +'" fullwidth="'+ photo.width +'"></a>';
+                });
+
+                if(html !== '') {
+                    _this.photoContainer.html(html);
+                    $('a.fbPic').click(function() {
+                        $('a.fbPic').removeClass('selected');
+                        $(this).addClass('selected');
+                        _this.onPhotoClick($(this).find('img'));
+                    });
+                }
+            });
+        },
+
+        showBreadcrumb: function(albumName) {
+            document.getElementById('fbCurrentAlbum').innerHTML = albumName;
+            this.breadcrumb.style.display = 'block';
         },
 
         unload: function(callback) {
